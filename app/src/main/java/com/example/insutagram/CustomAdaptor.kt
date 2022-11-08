9862cfc1a72902af31c4886c925cebde743c5b36
10000DOO <gunjoun99@gmail.com>
2022-11-08 13:25:10 +0900

    어댑터 연결 전 db에서 값 넘어오는 것 확인

--- app/src/main/java/com/example/insutagram/CustomAdaptor.kt
+++ app/src/main/java/com/example/insutagram/CustomAdaptor.kt
@@ -1,37 +1,41 @@
 package com.example.insutagram
-
+import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.insutagram.databinding.PostBinding
 import com.google.firebase.firestore.QueryDocumentSnapshot
-import java.util.HashMap
 
-class CustomAdapter(private val viewModel: MyViewModel) :
-    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
-    inner class ViewHolder(private val binding: PostBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun setContents(pos: Int){
-            val item = viewModel.items[pos]
-            binding.userId.text = item.userId
-            binding.likeCount.text = "좋아요 ${item.likeCount}"
-            binding.commentCount.text = "댓글 ${item.commentsCount}"
-        }
+data class ContentDTO( var userId: String, var likeCount: String, var commentsCount: String){
+    constructor(doc: QueryDocumentSnapshot) :
+            this(doc["userId"].toString(), doc["likeCount"].toString(), doc["commentCount"].toString())
+}
+
+class ViewHolder(val binding: PostBinding) : RecyclerView.ViewHolder(binding.root)
+
+class CustomAdapter(private val context: Context, private var items: List<ContentDTO>) :
+    RecyclerView.Adapter<ViewHolder>() {
+
+    fun updateList(newList: List<ContentDTO>) {
+        items = newList
+        notifyDataSetChanged()
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = PostBinding.inflate(layoutInflater, parent,false)
-        val viewHolder = ViewHolder(binding)
-        return viewHolder
+        val binding = PostBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.setContents(position)
+        var item = items[position]
+        holder.binding.userId.text = item.userId
+        holder.binding.likeCount.text = item.likeCount
+        holder.binding.commentCount.text = item.commentsCount
     }
 
     override fun getItemCount(): Int {
-        return viewModel.items.size
+        return items.size
     }
 }
--- app/src/main/java/com/example/insutagram/MainActivity.kt
+++ app/src/main/java/com/example/insutagram/MainActivity.kt
@@ -3,22 +3,17 @@ package com.example.insutagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.fragment.app.*
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.insutagram.databinding.ActivityMainBinding
-import com.example.insutagram.fragments.HomeFragment
-import com.example.insutagram.fragments.ProfileFragment
-import com.example.insutagram.fragments.SearchFragment
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-private const val SEARCH = "SearchFragment"
-private const val HOME = "HomeFragment"
-private const val PROFILE = "ProfileFragment"
 
 class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
+    private val db: FirebaseFirestore = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,58 +26,21 @@ class MainActivity : AppCompatActivity(){
             finish()
         }
 
-        setFragment(HOME,HomeFragment())
-        binding.navbar.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.home_icon -> setFragment(HOME, HomeFragment())
-                R.id.search_icon -> setFragment(SEARCH, SearchFragment())
-                R.id.profile_icon -> setFragment(PROFILE, ProfileFragment())
+//        binding.navbar.setOnItemSelectedListener { item ->
+//            when(item.itemId) {
+//                R.id.home_icon ->
+//                R.id.search_icon ->
+//                R.id.profile_icon ->
+//            }
+//            true
+//        }
+
+        val itemsCollectionRef = db.collection("content")
+        itemsCollectionRef.get().addOnSuccessListener {
+            var items = mutableListOf<ContentDTO>()
+            for (doc in it){
+                items.add(ContentDTO(doc))
             }
-            true
         }
     }
-
-    private fun setFragment(tag: String, fragment: Fragment) {
-        val manager: FragmentManager = supportFragmentManager
-        val fragTransaction = manager.beginTransaction()
-        if (manager.findFragmentByTag(tag) == null){
-            fragTransaction.add(R.id.contents, fragment, tag)
-        }
-
-        val profile = manager.findFragmentByTag(PROFILE)
-        val home = manager.findFragmentByTag(HOME)
-        val search = manager.findFragmentByTag(SEARCH)
-
-        if (profile != null){
-            fragTransaction.hide(profile)
-        }
-
-        if (home != null){
-            fragTransaction.hide(home)
-        }
-
-        if (search != null) {
-            fragTransaction.hide(search)
-        }
-
-        if (tag == SEARCH) {
-            if (search!=null){
-                fragTransaction.show(search)
-            }
-        }
-        else if (tag == HOME) {
-            if (home != null) {
-                fragTransaction.show(home)
-            }
-        }
-
-        else if (tag == PROFILE){
-            if (profile != null){
-                fragTransaction.show(profile)
-            }
-        }
-
-        fragTransaction.commitAllowingStateLoss()
-    }
-
 }
