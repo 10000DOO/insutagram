fa5ed916b3a54c81fbed6056fbfcb96c965e96cd
HwaRang <ghk2684@naver.com>
2022-11-08 12:35:46 +0900

    게시물 업로드 기능 구현
    디자인 수정 예정

--- app/src/main/java/com/example/insutagram/MainActivity.kt
+++ app/src/main/java/com/example/insutagram/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.insutagram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //글쓰기 화면으로 이동
+        val fab: View = findViewById(R.id.post_btn)
+        fab.setOnClickListener { view ->
+
+            val intent = Intent(this, PostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/insutagram/PostActivity.kt
@@ -0,0 +1,100 @@
+package com.example.insutagram
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
+
+class PostActivity : AppCompatActivity() {
+
+    lateinit var storage : FirebaseStorage
+    lateinit var auth : FirebaseAuth
+    lateinit var firestore : FirebaseFirestore
+    var photoUri : Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+
+        //초기화
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+
+        var photoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            //사진 받는 부분
+            photoUri = result.data?.data
+            findViewById<ImageView>(R.id.imageView2).setImageURI(photoUri)
+        }
+
+        //사진추가 버튼
+        findViewById<Button>(R.id.addphoto_btn_upload).setOnClickListener {
+            photoResult.launch(photoPickerIntent)
+        }
+
+        //게시 버튼
+        findViewById<Button>(R.id.post_btn).setOnClickListener {
+            contentUpload()
+        }
+
+
+    }
+
+    //게시하기
+    fun contentUpload(){
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "IMAGE_" + timestamp + ".png"
+        var storagePath = storage.reference?.child("images")?.child(imageFileName)
+
+        if (storagePath != null) {
+            storagePath.putFile(photoUri!!).continueWithTask {
+                return@continueWithTask storagePath.downloadUrl
+            }.addOnCompleteListener {
+                    downloadUrl ->
+
+                var DBModel = DBModel()
+
+                DBModel.imageUrl = downloadUrl.result.toString()
+                DBModel.post_text = findViewById<EditText>(R.id.addphoto_edit_explain).text.toString()
+                DBModel.uid = auth?.currentUser?.uid
+                DBModel.userId = auth?.currentUser?.email
+                DBModel.timestamp = System.currentTimeMillis()
+
+                //collection(Collection 이름). document(Doucument 이름/ 아무것도 안넣으면 무작위 생성).set(입력할 데이터)
+                firestore.collection("images").document().set(DBModel)
+
+                Toast.makeText(this,"업로드에 성공하였습니다.", Toast.LENGTH_LONG).show()
+
+                //페이지 나오기. 액티비티 종료
+                //finish()
+
+            }
+        }
+
+    }
+    fun getImageUri(context : Context, bitmap : Bitmap): Uri? {
+        var bytes = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,bytes)
+        var path = MediaStore.Images.Media.insertImage(context.contentResolver,bitmap,"Title",null)
+        return Uri.parse(path)
+    }
+    
+}
