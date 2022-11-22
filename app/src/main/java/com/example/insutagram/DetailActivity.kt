0a2ae57b65c25448587cca95960ba590c2116976
10000DOO <gunjoun99@gmail.com>
2022-11-22 20:40:51 +0900

    상세화면 수정

--- app/src/main/java/com/example/insutagram/DetailActivity.kt
+++ app/src/main/java/com/example/insutagram/DetailActivity.kt
@@ -1,91 +1,64 @@
 package com.example.insutagram
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.fragment.app.*
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.insutagram.databinding.ActivitySearchBinding
+import com.example.insutagram.databinding.ActivityDetailBinding
+import com.example.insutagram.databinding.ActivityMainBinding
 import com.example.insutagram.databinding.PostDetailBinding
-import com.example.insutagram.dto.Content
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-
-class DetailActivity: AppCompatActivity() {
-
-    private val db: FirebaseFirestore = Firebase.firestore
-    private lateinit var binding: PostDetailBinding
-    var storage = Firebase.storage
-    private val testCollectionRef = db.collection("test")
-    private val followCollectionRef = db.collection("follow")
-    private val itemsCollectionRef = db.collection("images")
-    var favoriteClick = HashMap<String,Boolean>()
-    var currentUid = FirebaseAuth.getInstance().currentUser!!.uid
 
 
-    var uid: String? = null
+class DetailActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityDetailBinding
+    private var adapter: DetailAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = PostDetailBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //로그인이 안되어있으면 로그인 화면으로 이동
+        if (Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+            finish()
+        }
 
-        //val value1 = intent?.getStringExtra("key1")
-        val user = intent.getSerializableExtra("key1") as Content? // 직렬화된 객체를 받는 방법
-        val postID = intent.getStringExtra("postID")
-        println("$$$$$$$$$$"+postID)
-        // 프로필 이름 출력
-        binding.userName.text= user!!.userId
-        // 프로필 사진 출력
-        testCollectionRef.document(user.uid!!).get().addOnSuccessListener {
-            val imageRef2 = storage.getReferenceFromUrl(it["profile_img"].toString())
-            imageRef2.getBytes(Long.MAX_VALUE).addOnCompleteListener{//successListener도 가능
-                if(it.isSuccessful){
-                    val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
-                    binding.userImage.setImageBitmap(bmp)
-                }
+        binding.navbar.setOnItemSelectedListener { item ->
+            if (item.itemId == R.id.home_icon){
+                startActivity(Intent(this, MainActivity::class.java))
+            }else if (item.itemId == R.id.add_post){
+                startActivity(Intent(this, PostActivity::class.java))
             }
-        }
-        // 게시글 사진 출력
-        itemsCollectionRef.document(postID!!).get().addOnSuccessListener {
-            val imageRef2 = storage.getReferenceFromUrl(it["imageUrl"].toString())
-            imageRef2.getBytes(Long.MAX_VALUE).addOnCompleteListener{//successListener도 가능
-                if(it.isSuccessful){
-                    val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
-                    binding.postMainImage.setImageBitmap(bmp)
-                }
+            else if (item.itemId == R.id.search_icon){
+                startActivity(Intent(this, SearchActivity::class.java))
             }
-        }
-        //좋아요
-        binding.likeCount.text = "좋아요 ${user.favoriteCount}"
-        if (user.favoriteCheck.get(currentUid) == true){
-            binding.favoriteButton.setImageResource(R.drawable.ic_favorite)
-        }else{
-            binding.favoriteButton.setImageResource(R.drawable.ic_favorite_border)
-        }
-        binding.favoriteButton.setOnClickListener {
-            if (user.favoriteCheck.get(currentUid) == null || user.favoriteCheck.get(currentUid) == false){
-                binding.favoriteButton.setImageResource(R.drawable.ic_favorite)
-                //itemsCollectionRef.document(postID).update("favoriteCount",(user.favoriteCount?.toInt()?.plus(1)))
-                itemsCollectionRef.document(postID).update("favoriteClick",user.favoriteCount+1)
-                favoriteClick.put(currentUid, true)
-                itemsCollectionRef.document(postID).update("favoriteCheck",favoriteClick)
-                CustomAdapter(this, emptyList()).updatePost()
-            }else{
-                binding.favoriteButton.setImageResource(R.drawable.ic_favorite_border)
-                itemsCollectionRef.document(postID).update("favoriteCount",(user.favoriteCount?.toInt()?.minus(1)))
-                favoriteClick.put(currentUid, false)
-                itemsCollectionRef.document(postID).update("favoriteCheck",favoriteClick)
-                CustomAdapter(this, emptyList()).updatePost()
+            else if (item.itemId == R.id.profile_icon){
+                startActivity(Intent(this, ProfileActivity::class.java))
             }
+            true
         }
+        binding.contents.layoutManager = LinearLayoutManager(this)
+        adapter = DetailAdapter(this, emptyList())
+        binding.contents.adapter = adapter
 
 
-
-        //본문 내용 출력
-        binding.postMainContent.text= user!!.post_text
-
+        adapter!!.updatePost()
+        //adapter!!.queryWhere()
     }
 }
--- app/src/main/java/com/example/insutagram/DetailAdapter.kt
+++ app/src/main/java/com/example/insutagram/DetailAdapter.kt
@@ -1,16 +1,17 @@
 package com.example.insutagram
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.getIntent
 import android.graphics.BitmapFactory
+import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.insutagram.databinding.PostBinding
+import com.example.insutagram.databinding.PostDetailBinding
 import com.example.insutagram.dto.Content
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,19 +19,19 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
+
 private val db: FirebaseFirestore = Firebase.firestore
 private val itemsCollectionRef = db.collection("images")
 private val followCollectionRef = db.collection("follow")
 private val testCollectionRef = db.collection("test")
 private var currentUid = FirebaseAuth.getInstance().currentUser!!.uid
 
-class ViewHolder(val binding: PostBinding) : RecyclerView.ViewHolder(binding.root)
+class DetailViewHolder(val binding: PostDetailBinding) : RecyclerView.ViewHolder(binding.root)
+
+class DetailAdapter(private val context: Context, private var items: List<Content>) :
 
 
-class CustomAdapter(private val context: Context, private var items: List<Content>) :
-
-
-    RecyclerView.Adapter<ViewHolder>() {
+    RecyclerView.Adapter<DetailViewHolder>() {
 
     var storage = Firebase.storage
     var contentUidList = arrayListOf<String>()
@@ -44,69 +45,59 @@ class CustomAdapter(private val context: Context, private var items: List<Conten
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = PostBinding.inflate(layoutInflater, parent, false)
-        return ViewHolder(binding)
+        val binding = PostDetailBinding.inflate(layoutInflater, parent, false)
+        return DetailViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //println("%%%%%%%%%%%%%%%"+contentUidList[position])
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
         var item = items[position]
         testCollectionRef.document(item.uid!!).get().addOnSuccessListener {
             val imageRef2 = storage.getReferenceFromUrl(it["profile_img"].toString())
             imageRef2.getBytes(Long.MAX_VALUE).addOnCompleteListener{//successListener도 가능
                 if(it.isSuccessful){
                     val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
-                    holder.binding.profileImage.setImageBitmap(bmp)
+                    holder.binding.userImage.setImageBitmap(bmp)
                 }
             }
         }
-        holder.binding.userId.text = item.userId
+        holder.binding.userName.text = item.userId
         holder.binding.likeCount.text = "좋아요 ${item.favoriteCount}"
-        holder.binding.postContent.text = item.post_text
+        holder.binding.postMainContent.text = item.post_text
         //게시글 클릭시 상세 페이지로 이동
 
-        holder.binding.contentImage.setOnClickListener{
-            println(item.uid)
-            val intent = Intent(holder.binding.root.context,DetailActivity::class.java)
-            println(intent)
-            holder.binding.root.context.startActivity(intent)
-
-            //startActivity(context,intent,null)
-            //intent.run{binding.root.context.startActivity(this)}
-        }
         itemsCollectionRef.document(contentUidList[position]).get().addOnSuccessListener {
             val imageRef2 = storage.getReferenceFromUrl(it["imageUrl"].toString())
             imageRef2.getBytes(Long.MAX_VALUE).addOnCompleteListener{//successListener도 가능
                 if(it.isSuccessful){
                     val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
-                    holder.binding.contentImage.setImageBitmap(bmp)
+                    holder.binding.postMainImage.setImageBitmap(bmp)
                 }
             }
 
         }
         //fun checkLike(){
-            if (item.favoriteCheck.get(currentUid) == true){
+        if (item.favoriteCheck.get(currentUid) == true){
+            holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite)
+        }else{
+            holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite_border)
+        }
+        holder.binding.favoriteButton.setOnClickListener {
+            if (item.favoriteCheck.get(currentUid) == null || item.favoriteCheck.get(currentUid) == false){
                 holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite)
+                itemsCollectionRef.document(contentUidList[position]).update("favoriteCount",(item.favoriteCount?.toInt()?.plus(1)))
+                favoriteClick.put(currentUid, true)
+                itemsCollectionRef.document(contentUidList[position]).update("favoriteCheck",favoriteClick)
+                updatePost()
             }else{
                 holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite_border)
+                itemsCollectionRef.document(contentUidList[position]).update("favoriteCount",(item.favoriteCount?.toInt()?.minus(1)))
+                favoriteClick.put(currentUid, false)
+                itemsCollectionRef.document(contentUidList[position]).update("favoriteCheck",favoriteClick)
+                updatePost()
             }
-            holder.binding.favoriteButton.setOnClickListener {
-                if (item.favoriteCheck.get(currentUid) == null || item.favoriteCheck.get(currentUid) == false){
-                    holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite)
-                    itemsCollectionRef.document(contentUidList[position]).update("favoriteCount",(item.favoriteCount?.toInt()?.plus(1)))
-                    favoriteClick.put(currentUid, true)
-                    itemsCollectionRef.document(contentUidList[position]).update("favoriteCheck",favoriteClick)
-                    updatePost()
-                }else{
-                    holder.binding.favoriteButton.setImageResource(R.drawable.ic_favorite_border)
-                    itemsCollectionRef.document(contentUidList[position]).update("favoriteCount",(item.favoriteCount?.toInt()?.minus(1)))
-                    favoriteClick.put(currentUid, false)
-                    itemsCollectionRef.document(contentUidList[position]).update("favoriteCheck",favoriteClick)
-                    updatePost()
-                }
-            }
+        }
         //}
         //checkLike()
 
@@ -167,5 +158,6 @@ class CustomAdapter(private val context: Context, private var items: List<Conten
             updateList(itemss)
         }
     }
-
 }
+
+
